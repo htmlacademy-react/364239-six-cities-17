65d92074bd1c205926412c53040649a6bb93a2ca
Vasilii <homenada@yandex.ru>
2024-12-26 20:14:35 +0300

    non-saved

--- src/mocks/reviews.ts
+++ src/mocks/reviews.ts
@@ -2,4 +2,37 @@ import { ReviewType } from '../types/offer';
 
 export const REVIEW_MOCKS: ReviewType[] =
 [
+  {
+    'id': '7707fdb9-afec-4653-a6f1-4cfe0db8aee6',
+    'comment': 'The deluxe room was a quite comfortable one with all the adequate facilities. The only thing that made me feel uncomfortable was the rude behavior of an impolite staff at the reception desk.',
+    'date': '2024-12-01T21:00:00.096Z',
+    'rating': 3,
+    'user': {
+      'name': 'Christina',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/avatar/9.jpg',
+      'isPro': false
+    }
+  },
+  {
+    'id': '7d56f513-8791-45fb-a2e7-56ef8ddc7b95',
+    'comment': 'We loved it so much, the house, the veiw, the location just great.. Highly reccomend :)',
+    'date': '2024-11-30T21:00:00.096Z',
+    'rating': 2,
+    'user': {
+      'name': 'Mollie',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/avatar/5.jpg',
+      'isPro': true
+    }
+  },
+  {
+    'id': '5c2b8988-e6c7-4f4b-9431-a59073b11a83',
+    'comment': 'This villa is perfect in every way: the view on mountains and waterfalls, the hot tub and the villa itself. The evening here became a great continuation of our journeys over country.',
+    'date': '2024-11-28T21:00:00.096Z',
+    'rating': 5,
+    'user': {
+      'name': 'Zak',
+      'avatarUrl': 'https://16.design.htmlacademy.pro/static/avatar/8.jpg',
+      'isPro': false
+    }
+  }
 ];
